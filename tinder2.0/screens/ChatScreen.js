db8282a9921101cb7044b70345345f7e1aa9a8da
slimkhemiri <khemiri.medslim1@gmail.com>
2021-12-05 16:15:44 +0000

    navigation between screens

---
+++ tinder2.0/screens/ChatScreen.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const ChatScreen = () => {
+    return (
+        <View>
+            <Text>chatScreen</Text>
+        </View>
+    )
+}
+
+export default ChatScreen
